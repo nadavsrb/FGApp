780ce65f7c367fce0f5cab364c0bc4f87efbcefc
NADAV SHARABI <nadav.sharabi1@live.biu.ac.il>
2021-06-17 13:24:25 +0300

    starting

--- app/src/main/java/com/example/fgapp/view/MainActivity.kt
+++ app/src/main/java/com/example/fgapp/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.fgapp
+package com.example.fgapp.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fgapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
